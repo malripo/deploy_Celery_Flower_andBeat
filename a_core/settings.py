1cdff6f9824abb5817dd2862b474c75997e13a48
malripo <malripo741205@gmail.com>
2024-11-19 16:45:24 +0900

    Update settings.py

--- a_core/settings.py
+++ a_core/settings.py
@@ -41,14 +41,16 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'celeryflowerbeat.up.railway.app']
+site_domain = env('RAILWAY_PUNLIC_DOMAIN', default='')
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', site_domain]
 
 INTERNAL_IPS = (
     '127.0.0.1',
     'localhost:8000'
 )
 
-CSRF_TRUSTED_ORIGINS = [ 'https://celeryflowerbeat.up.railway.app' ]
+CSRF_TRUSTED_ORIGINS = [ f'https://{site_domain}' ]
 
 
 # Application definition
